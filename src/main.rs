e04a1d0869823cd8bdbe729def4c7fb23d711af3
Rajat Rajput <rajputrajat@gmail.com>
2020-11-23 23:58:43 +0530

    wip. code refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use log::{debug, warn};
+use log::debug;
 use log_panics;
 use serde::Deserialize;
 use simplelog::*;
@@ -46,22 +46,12 @@ fn map_args(json_data: &AssetRelocationDef, args: String) -> HashMap<String, Str
 }
 
 trait FileOperations {
-    fn create_hardlink(&self) -> Option<String>;
-    fn create_copy(&self) -> Option<String> {
-        None
-    }
-    fn create_move(&self) -> Option<String> {
-        None
-    }
-    fn remove_dst(&self) -> Option<String> {
-        None
-    }
-    fn create_dir(&self, _: &Path) -> Option<String> {
-        None
-    }
-    fn recurse(&self, _: fn(&Self) -> Option<String>) -> Option<String> {
-        None
-    }
+    fn create_hardlink(&self);
+    fn create_copy(&self);
+    fn create_move(&self);
+    fn remove_dst(&self);
+    fn create_dst_dir(&self);
+    fn common(&self) -> bool;
 }
 
 struct AssetPaths {
@@ -70,97 +60,101 @@ struct AssetPaths {
 }
 
 impl FileOperations for AssetPaths {
-    fn recurse(&self, cb: fn(&Self) -> Option<String>) -> Option<String> {
-        for item in WalkDir::new(&self.src) {
-            debug!("current item, src: \'{:?}\'", item);
-            let item = item.unwrap();
-            let src_path = &item.path();
-            let src_str = src_path.to_str().unwrap();
-            let src_delta = &src_str[self.src.to_str().unwrap().len()..];
-            let dst_str = self.dst.to_str().unwrap().to_owned() + src_delta;
-            let dst_path = Path::new(&dst_str);
-            if src_path.is_dir() {
-                debug!("\'{:?}\' is a dir, so create it", src_path);
-                self.create_dir(dst_path)?;
-            } else {
-                self.remove_dst();
-                cb(&self)?;
-            }
+    fn create_hardlink(&self) {
+        if self.common() {
+            debug!(
+                "creating hardlink from '{:?}' to '{:?}'",
+                self.src, self.dst
+            );
+            fs::hard_link(&self.src, &self.dst).expect("couldn't create hardlink");
         }
-        None
     }
-    fn create_dir(&self, d: &Path) -> Option<String> {
-        if !d.exists() {
-            debug!("creating dir: {:?}", d);
-            fs::create_dir_all(d).expect("couldn't create dir");
+
+    fn create_copy(&self) {
+        if self.common() {
+            debug!("copying from '{:?}' to '{:?}'", self.src, self.dst);
+            let _ = fs::copy(&self.src, &self.dst).expect("problem while copying");
         }
-        None
     }
 
-    fn create_hardlink(&self) -> Option<String> {
-        debug!(
-            "creating hardlink from '{:?}' to '{:?}'",
-            self.src, self.dst
-        );
-        fs::hard_link(&self.src, &self.dst).expect("couldn't create hardlink");
-        None
+    fn create_move(&self) {
+        if self.common() {
+            debug!("moving from '{:?}' to '{:?}'", self.src, self.dst);
+            fs::rename(&self.src, &self.dst).expect("move operation failed");
+        }
     }
 
-    fn create_copy(&self) -> Option<String> {
-        debug!("copying from '{:?}' to '{:?}'", self.src, self.dst);
-        let _ = fs::copy(&self.src, &self.dst).expect("problem while copying");
-        None
+    fn common(&self) -> bool {
+        if self.src.is_dir() {
+            self.create_dst_dir();
+            false
+        } else {
+            self.remove_dst();
+            true
+        }
     }
-
-    fn create_move(&self) -> Option<String> {
-        debug!("moving from '{:?}' to '{:?}'", self.src, self.dst);
-        fs::rename(&self.src, &self.dst).expect("move operation failed");
-        None
+    fn create_dst_dir(&self) {
+        if !self.dst.exists() {
+            debug!("creating dir: {:?}", self.dst);
+            fs::create_dir_all(&self.dst).expect("couldn't create dir");
+        }
     }
-
-    fn remove_dst(&self) -> Option<String> {
+    fn remove_dst(&self) {
         if self.dst.exists() {
             debug!("removing file '{:?}'", self.dst);
             fs::remove_file(&self.dst).expect("error in file deletion");
         }
-        None
     }
 }
 
 fn get_paths_after_replacing_args(
     job: &JobConfigs,
     args_map: &HashMap<String, String>,
-) -> (PathBuf, PathBuf) {
+) -> (String, String) {
     let mut src = job.src.clone();
     let mut dst = job.dst.clone();
     for (arg, val) in args_map {
         src = src.replace(arg, val.as_str());
         dst = dst.replace(arg, val);
     }
-    (PathBuf::from(src.as_str()), PathBuf::from(dst.as_str()))
+    (src, dst)
 }
 
 fn do_jobs(json_data: &AssetRelocationDef, args_map: &HashMap<String, String>) {
     for job in &json_data.jobs {
         let (src, dst) = get_paths_after_replacing_args(job, args_map);
-        let asset_paths = AssetPaths { src, dst };
-        let _ = do_job(job, &asset_paths);
+        do_job(src.as_str(), dst.as_str(), job.todo.as_str());
     }
 }
 
-fn do_job(job: &JobConfigs, asset_paths: &impl FileOperations) -> Option<String> {
-    debug!("current job is: {:?}", job);
-    //asset_paths.remove_dst();
-    if job.src.as_str() == "" && job.dst.as_str() == "" {
-        warn!("paths shouldn't be empty. ignoring for now");
-        return None;
-    }
-    match job.todo.as_str() {
-        "hardlink" => asset_paths.recurse(FileOperations::create_hardlink),
-        "copy" => asset_paths.recurse(FileOperations::create_copy),
-        "move" => asset_paths.recurse(FileOperations::create_move),
+fn do_job(src: &str, dst: &str, todo: &str) {
+    debug!("current job is: {:?}", todo);
+    let paths = get_asset_paths_for_processing(src, dst);
+    match todo {
+        "hardlink" => paths.iter().for_each(|x| x.create_hardlink()),
+        "copy" => paths.iter().for_each(|x| x.create_copy()),
+        "move" => paths.iter().for_each(|x| x.create_move()),
         _ => panic!("this is new. not yet handled"),
     }
+}
+
+fn get_asset_paths_for_processing(src: &str, dst: &str) -> Vec<AssetPaths> {
+    let mut paths: Vec<AssetPaths> = Vec::new();
+    for item in WalkDir::new(Path::new(src)) {
+        debug!("current item, src: \'{:?}\'", item);
+        let item = item.unwrap();
+        let src_path = &item.path();
+        let src_str = src_path.to_str().unwrap();
+        let src_delta = &src_str[src.len()..];
+        let dst_str = dst.to_owned() + src_delta;
+        let dst_path = Path::new(&dst_str);
+        let apath = AssetPaths {
+            src: src_path.to_path_buf(),
+            dst: dst_path.to_path_buf(),
+        };
+        paths.push(apath);
+    }
+    paths
 }
 
 fn setup_logger() {
@@ -189,7 +183,6 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::fmt::Write;
     #[test]
     fn json_parsing() {
         let value = parse_json(Path::new("asset_relocation_def.json"));
@@ -218,69 +211,5 @@ mod tests {
         assert_eq!(args_map["{Configuration}"], "one");
         assert_eq!(args_map["{MonacoBinDir}"], "seven");
         assert_eq!(args_map["{ProjectDir}"], "four");
-    }
-
-    struct TestAssetPaths {
-        src: PathBuf,
-        dst: PathBuf,
-    }
-    impl FileOperations for TestAssetPaths {
-        fn create_hardlink(&self) -> Option<String> {
-            let mut ret = String::new();
-            write!(
-                &mut ret,
-                "hardlink, {}, {}",
-                self.src.to_str().unwrap(),
-                self.dst.to_str().unwrap()
-            )
-            .expect("?");
-            Some(ret)
-        }
-        fn create_copy(&self) -> Option<String> {
-            let mut ret = String::new();
-            write!(
-                &mut ret,
-                "copy, {}, {}",
-                self.src.to_str().unwrap(),
-                self.dst.to_str().unwrap()
-            )
-            .expect("?");
-            Some(ret)
-        }
-    }
-
-    #[test]
-    fn which_operation() {
-        let args = "one, two, three, four, five, six, seven";
-        let json_data = parse_json(Path::new("asset_relocation_def.json"));
-        let args_map = map_args(&json_data, String::from(args));
-        {
-            let job = &json_data.jobs[0];
-            let (src, dst) = get_paths_after_replacing_args(job, &args_map);
-            let test_asset_paths = TestAssetPaths { src, dst };
-            assert_eq!(
-                "hardlink, four/assets/setup.txt, six/setup.txt",
-                do_job(job, &test_asset_paths).unwrap()
-            );
-        }
-        {
-            let job = &json_data.jobs[4];
-            let (src, dst) = get_paths_after_replacing_args(job, &args_map);
-            let test_asset_paths = TestAssetPaths { src, dst };
-            assert_eq!(
-                concat!(
-                    "hardlink, ",
-                    "six/../../../../Tools/GDKRuntimeHost/two/one, ",
-                    "seven/one/Runtime/bin"
-                ),
-                do_job(job, &test_asset_paths).unwrap()
-            );
-        }
-        {
-            let job = &json_data.jobs[7];
-            let (src, dst) = get_paths_after_replacing_args(job, &args_map);
-            let test_asset_paths = TestAssetPaths { src, dst };
-            assert_eq!("copy, , ", do_job(job, &test_asset_paths).unwrap());
-        }
     }
 }

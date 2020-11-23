573241febc77c92e437be56730a1977f9afde44b
Rajat Rajput <rajputrajat@gmail.com>
2020-11-23 19:51:35 +0530

    commiting before making changes

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::collections::HashMap;
 use std::env;
 use std::fs;
 use std::path::{Path, PathBuf};
+use walkdir::WalkDir;
 
 #[derive(Deserialize)]
 struct AssetRelocationDef {
@@ -44,9 +45,7 @@ fn map_args(json_data: &AssetRelocationDef, args: String) -> HashMap<String, Str
 }
 
 trait FileOperations {
-    fn create_hardlink(&self) -> Option<String> {
-        None
-    }
+    fn create_hardlink(&self) -> Option<String>;
     fn create_copy(&self) -> Option<String> {
         None
     }
@@ -54,6 +53,12 @@ trait FileOperations {
         None
     }
     fn remove_dst(&self) -> Option<String> {
+        None
+    }
+    fn create_dir(&self, _: &Path) -> Option<String> {
+        None
+    }
+    fn recurse(&self, _: fn(&Self) -> Option<String>) -> Option<String> {
         None
     }
 }
@@ -64,6 +69,30 @@ struct AssetPaths {
 }
 
 impl FileOperations for AssetPaths {
+    fn recurse(&self, cb: fn(&Self) -> Option<String>) -> Option<String> {
+        for item in WalkDir::new(&self.src) {
+            let item = item.unwrap();
+            let src_path = &item.path();
+            let src_str = src_path.to_str().unwrap();
+            let src_delta = &src_str[self.src.to_str().unwrap().len()..];
+            let dst_str = self.dst.to_str().unwrap().to_owned() + src_delta;
+            let dst_path = Path::new(&dst_str);
+            if src_path.is_dir() {
+                self.create_dir(dst_path)?;
+            } else {
+                cb(&self)?;
+            }
+        }
+        None
+    }
+    fn create_dir(&self, d: &Path) -> Option<String> {
+        if !d.exists() {
+            debug!("creating dir: {:?}", d);
+            fs::create_dir(d).expect("couldn't create dir");
+        }
+        None
+    }
+
     fn create_hardlink(&self) -> Option<String> {
         debug!(
             "creating hardlink from '{:?}' to '{:?}'",
@@ -94,7 +123,10 @@ impl FileOperations for AssetPaths {
     }
 }
 
-fn get_src_dst_paths(job: &JobConfigs, args_map: &HashMap<String, String>) -> (PathBuf, PathBuf) {
+fn get_paths_after_replacing_args(
+    job: &JobConfigs,
+    args_map: &HashMap<String, String>,
+) -> (PathBuf, PathBuf) {
     let mut src = job.src.clone();
     let mut dst = job.dst.clone();
     for (arg, val) in args_map {
@@ -106,7 +138,7 @@ fn get_src_dst_paths(job: &JobConfigs, args_map: &HashMap<String, String>) -> (P
 
 fn do_jobs(json_data: &AssetRelocationDef, args_map: &HashMap<String, String>) {
     for job in &json_data.jobs {
-        let (src, dst) = get_src_dst_paths(job, args_map);
+        let (src, dst) = get_paths_after_replacing_args(job, args_map);
         let asset_paths = AssetPaths { src, dst };
         let _ = do_job(job, &asset_paths);
     }
@@ -116,9 +148,9 @@ fn do_job(job: &JobConfigs, asset_paths: &impl FileOperations) -> Option<String>
     debug!("current job is: {:?}", job);
     asset_paths.remove_dst();
     match job.todo.as_str() {
-        "hardlink" => asset_paths.create_hardlink(),
-        "copy" => asset_paths.create_copy(),
-        "move" => asset_paths.create_move(),
+        "hardlink" => asset_paths.recurse(FileOperations::create_hardlink),
+        "copy" => asset_paths.recurse(FileOperations::create_copy),
+        "move" => asset_paths.recurse(FileOperations::create_move),
         _ => panic!("this is new. not yet handled"),
     }
 }
@@ -136,7 +168,6 @@ fn setup_logger() {
 }
 
 fn main() {
-    //"asset_relocation_def.json"
     setup_logger();
     let json_data = parse_json(Path::new("asset_relocation_def.json"));
     let args = env::args().skip(1).next().unwrap();
@@ -216,7 +247,7 @@ mod tests {
         let args_map = map_args(&json_data, String::from(args));
         {
             let job = &json_data.jobs[0];
-            let (src, dst) = get_src_dst_paths(job, &args_map);
+            let (src, dst) = get_paths_after_replacing_args(job, &args_map);
             let test_asset_paths = TestAssetPaths { src, dst };
             assert_eq!(
                 "hardlink, four/assets/setup.txt, six/setup.txt",
@@ -225,7 +256,7 @@ mod tests {
         }
         {
             let job = &json_data.jobs[4];
-            let (src, dst) = get_src_dst_paths(job, &args_map);
+            let (src, dst) = get_paths_after_replacing_args(job, &args_map);
             let test_asset_paths = TestAssetPaths { src, dst };
             assert_eq!(
                 concat!(
@@ -238,7 +269,7 @@ mod tests {
         }
         {
             let job = &json_data.jobs[7];
-            let (src, dst) = get_src_dst_paths(job, &args_map);
+            let (src, dst) = get_paths_after_replacing_args(job, &args_map);
             let test_asset_paths = TestAssetPaths { src, dst };
             assert_eq!("copy, , ", do_job(job, &test_asset_paths).unwrap());
         }

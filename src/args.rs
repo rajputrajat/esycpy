250894eef3072beb89de6caff15c31afb05321e6
Rajat Rajput <rajputrajat@gmail.com>
2021-01-31 14:43:20 +0530

    command line args are done

--- src/args.rs
+++ src/args.rs
@@ -1,13 +1,20 @@
-use clap::{Arg, ArgGroup, App, SubCommand};
+use clap::{Arg, App, SubCommand};
+
+pub enum Operation {
+    Copy_,
+    Move,
+    Hardlink
+}
 
 pub enum ArgsType {
     CmdLine {
+        op: Operation,
         from: String,
         to: String,
     },
     Json {
         json_file: String,
-        variables: Option<Vec<String>>
+        variables: Option<Vec<(String, String)>>
     }
 }
 
@@ -15,11 +22,13 @@ pub fn get_args() {
     let arg_from = Arg::with_name("from")
         .short("s")
         .long("from")
+        .takes_value(true)
         .value_name("source_path")
         .required(true);
     let arg_to = Arg::with_name("to")
         .short("d")
         .long("to")
+        .takes_value(true)
         .value_name("destination_path")
         .required(true);
     let matches = App::new("EsyCpy")
@@ -38,5 +47,13 @@ pub fn get_args() {
             .about( "create hardlinks of file/s from source to destination")
             .arg(arg_from)
             .arg(arg_to))
+        .arg(Arg::with_name("json_file")
+            .short("j")
+            .long("json")
+            .help("json input file path which defines copy/move/hardlink operations")
+            .takes_value(true)
+            .value_name("json_file_path")
+            .required(true))
+        .arg_from_usage("-v, --variables=<VARIABLE_NAME_VALUE_PAIR>... 'var name - value pairs'")
         .get_matches();
 }
